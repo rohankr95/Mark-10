5afc7950e62b109069709dc66676860b4e39e996
Rohan <rohankr95@gmail.com>
2021-08-24 17:17:03 +0530

    completed mark-10

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const notes = document.querySelectorAll(".notes");
 
+const denominations =[2000,500,100,20,10,5,1];
+
+
+checkButton.addEventListener("click",function(){
+    message.style.display="none";
+    if(billAmount.value>0){
+        if(cashGiven.value >= billAmount.value){
+           const returnAmount = cashGiven.value - billAmount.value;
+            calculateChange(returnAmount);
+
+        }else{
+
+            printErrorMessage("Do you want to wash plates?");
+        }
+
+    }else{
+        printErrorMessage("Bill amount is invalid");
+    }
+});
+
+function calculateChange(returnAmount){
+
+    for(let i=0;i<denominations.length;i++){
+        const noOfNotes = Math.trunc(returnAmount/denominations[i]);
+        returnAmount %= denominations[i];
+        notes[i].innerText=noOfNotes;
+    }
+
+}
+
+function printErrorMessage(textMessage){
+    message.style.display="block";
+    message.innerText = textMessage;
+}
